aeadc9a7bfddb07084834f0fbb79ab6cfe6eed72
iamansingh0 <amankumar7355197337@gmail.com>
2025-03-12 15:10:34 +0530

    Enhance HeroSection with image effects and responsive styles

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,45 +1,54 @@
+"use client";
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
 
 const HeroSection = () => {
-  return (
-    <section className='w-full pt-36 md:pt-48 pb-10'>
-        <div className='space-y-6 text-center'>
-            <div className='space-y-6 mx-auto'>
-                <h1 className='text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title'>
-                    Your AI Career Coach for
-                    <br/>
-                    Professional Success
-                </h1>
-                <p>
-                    Advance your career with personalised guidance, interview prep, and AI-powered tools for job success.
-                </p>
-            </div>
-            
-            <div>
-                <Link href='/dashboard'>
-                    <Button size='lg' className='px-8 cursor-pointer'>
-                        Get Started
-                    </Button>
-                </Link>
-            </div>
-            <div>
-                <div>
-                    <Image 
-                        src={"/banner.jpeg"}
-                        width={1080}
-                        height={720}
-                        alt='Banner image'
-                        className='rounded-lg shadow-2xl border mx-auto'
-                        priority
-                    />
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const scrollPosition = window.screenY;
+        const scrollThreshold = 100;
+    }, []) 
+
+    return (
+        <section className='w-full pt-36 md:pt-48 pb-10' suppressHydrationWarning>
+            <div className='space-y-6 text-center'>
+                <div className='space-y-6 mx-auto'>
+                    <h1 className='text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title'>
+                        Your AI Career Coach for
+                        <br />
+                        Professional Success
+                    </h1>
+                    <p className='mx-auto max-w-[600px] text-muted-foreground md:text-xl'>
+                        Advance your career with personalised guidance, interview prep, and AI-powered tools for job success.
+                    </p>
+                </div>
+
+                <div className='flex justify-center items-center'>
+                    <Link href='/dashboard'>
+                        <Button size='lg' className='px-8 cursor-pointer'>
+                            Get Started
+                        </Button>
+                    </Link>
+                </div>
+                <div className='hero-image-wrapper mt-5 md:mt-0'>
+                    <div ref={imageRef} className='hero-image'>
+                        <Image
+                            src={"/banner.jpeg"}
+                            width={1080}
+                            height={720}
+                            alt='Banner image'
+                            className='rounded-lg shadow-2xl border mx-auto'
+                            priority
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default HeroSection
