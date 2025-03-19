2172e7db261487b8de997732f24426a7a4752723
iamansingh0 <amankumar7355197337@gmail.com>
2025-03-19 13:59:22 +0530

    Add MainLayout component for consistent page structure

---
+++ app/(main)/layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MainLayout = ({children}) => {
+  // redirect user after onboarding
+  return (
+    <div className="mx-auto container mb-20 mt-24">{children}</div>
+  )
+}
+
+export default MainLayout
