053c44b830f84e5534b619dcd492c98cde17f5cb
iamansingh0 <amankumar7355197337@gmail.com>
2025-03-24 00:54:08 +0530

    Add assessment retrieval and display components for interview page

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { getAssessments } from '@/actions/interview'
+import StatsCards from './_components/stats-cards';
+import PerformanceChart from './_components/performance-chart';
+import QuizList from './_components/quiz-list';
 
-const InterviewPage = () => {
+const InterviewPage = async () => {
+  const assessments = await getAssessments();
   return (
-    <div>InterviewPage</div>
+    <div>
+      <div>
+        <h1 className='font-bold text-6xl gradient-title mb-5'>Interview Preparation</h1>
+        <div>
+          <StatsCards assessments={assessments}/>
+          <PerformanceChart assessments={assessments}/>
+          <QuizList assessments={assessments}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
