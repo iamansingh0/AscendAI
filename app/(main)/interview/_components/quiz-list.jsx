fba875f6188f8ebc3d67d4d004995a69be661d88
iamansingh0 <amankumar7355197337@gmail.com>
2025-03-24 01:42:35 +0530

    Refactor QuizList component to display recent quizzes with detailed performance metrics and improve user interaction

--- app/(main)/interview/_components/quiz-list.jsx
+++ app/(main)/interview/_components/quiz-list.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+"use client";
 
-const QuizList = () => {
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import QuizResult from "./quiz-result";
+
+const QuizList = ({ assessments }) => {
+  const router = useRouter();
+  const [selectedQuiz, setSelectedQuiz] = useState(null);
   return (
-    <div>QuizList</div>
-  )
-}
+    <>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between">
+          <div>
+            <CardTitle className="gradient-title text-3xl md:text-4xl">
+              Recent Quizzes
+            </CardTitle>
+            <CardDescription>Review your past quiz performance</CardDescription>
+          </div>
 
-export default QuizList
+          <Button
+            onClick={() => router.push("/interview/mock")}
+            className={"cursor-pointer"}
+          >
+            Start New Quiz
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            {assessments.map((a, index) => {
+              return (
+                <Card
+                  className={
+                    "cursor-pointer hover:bg-muted/50 transition-colors"
+                  }
+                  onClick={() => setSelectedQuiz(a)}
+                  key={a.id}
+                >
+                  <CardHeader>
+                    <CardTitle>Quiz {index + 1}</CardTitle>
+                    <CardDescription className={"flex justify-between w-full"}>
+                      <div>Score: {a.quizScore.toFixed(1)}%</div>
+                      <div>
+                        {format(new Date(a.createdAt), "MMMM dd, yyyy HH:mm")}
+                      </div>
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-muted-foreground text-sm">
+                      {a.improvementTip}
+                    </p>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        </CardContent>
+      </Card>
+
+      {/* dialog */}
+      <Dialog open={!!selectedQuiz} onOpenChange={() => setSelectedQuiz(null)}>
+        <DialogContent className={'max-w-3xl max-h-[90vh] overflow-y-auto'}>
+          <DialogHeader>
+            <DialogTitle></DialogTitle>
+          </DialogHeader>
+          <QuizResult
+            result={selectedQuiz}
+            onStartNew={() => router.push("/interview/mock")}
+            hideStartNew
+          />
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default QuizList;
+// https://youtu.be/UbXpRv5ApKA?t=14369
